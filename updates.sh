368ca621781986f9988b227c15390d297c71368a
DemiGodVoid <162964001+DemiGodVoid@users.noreply.github.com>
2024-08-08 01:09:20 -0500

    Create updates.sh

---
+++ updates.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Clear the terminal screen
+clear
+
+# Display update information
+echo "UPDATES"
+echo "------------------"
+echo "v1.0.0-alpha"
+echo "~~~~~~~~~~~"
+echo "Added: IP info lookup"
+echo ""
+echo "Added: Cam Phisher"
+echo "----------------------"
+
+# Wait for the user to press a key
+echo ""
+echo "Press any key to exit..."
+read -n 1 -s
