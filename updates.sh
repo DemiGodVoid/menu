80d6227c2c7f84081ecdf4ac6abcbeb7d370b92c
DemiGodVoid <162964001+DemiGodVoid@users.noreply.github.com>
2024-08-08 22:13:52 -0500

    Update updates.sh

--- updates.sh
+++ updates.sh
@@ -3,20 +3,24 @@
 # Clear the terminal screen
 clear
 
+# ANSI escape codes for red text
+RED='\033[0;31m'
+NC='\033[0m'  # No color
+
 # Display update information
-echo "UPDATES"
-echo "------------------"
-echo "v1.0.1-alpha"
-echo "Added: Kik Phishing Page"
-echo "------------------"
-echo "v1.0.0-alpha"
-echo "~~~~~~~~~~~"
-echo "Added: IP info lookup"
-echo "Added Gps pin pointer"
-echo "Added: Cam Phisher"
-echo "----------------------"
+echo -e "${RED}UPDATES${NC}"
+echo -e "${RED}------------------${NC}"
+echo -e "${RED}v1.0.1-alpha${NC}"
+echo -e "${RED}Added: Kik Phishing Page${NC}"
+echo -e "${RED}------------------${NC}"
+echo -e "${RED}v1.0.0-alpha${NC}"
+echo -e "${RED}~~~~~~~~~~~${NC}"
+echo -e "${RED}Added: IP info lookup${NC}"
+echo -e "${RED}Added: Gps pin pointer${NC}"
+echo -e "${RED}Added: Cam Phisher${NC}"
+echo -e "${RED}----------------------${NC}"
 
 # Wait for the user to press a key
 echo ""
-echo "Press any key to exit..."
+echo -e "${RED}Press any key to exit...${NC}"
 read -n 1 -s
