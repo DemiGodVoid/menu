56ec78ae649e6d846a6257dd2dff6e72b0523f56
DemiGodVoid <162964001+DemiGodVoid@users.noreply.github.com>
2024-08-13 23:23:23 -0500

    Update server4.js

--- tools/phishers/server4.js
+++ tools/phishers/server4.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'verify.html'));
+    res.sendFile(path.join(__dirname, 'public/verify.html'));
 });
 
 const port = 3000;
