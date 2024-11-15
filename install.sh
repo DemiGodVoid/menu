2a8524ca514f02dbed4cb49ff5b65844d4da5e39
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2024-11-15 19:22:08 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,42 +1,25 @@
 #!/bin/bash
 
-# Update package lists
-echo "Updating package lists..."
+wallet_dir="$(dirname "$(pwd)")"
+chmod +x "$wallet_dir/reload.sh"
+
+echo "Updating package list..."
 sudo apt-get update
 
-# Install pip if not already installed
-echo "Installing pip..."
-sudo apt-get install -y python3-pip
+echo "Installing Python and pip..."
+sudo apt-get install -y python3 python3-pip
 
-# Install the requests module using pip
-echo "Installing requests module..."
-pip3 install requests
+echo "Installing required Python packages..."
+sudo apt install python3-ecdsa 
+sudo apt install python3-cryptography 
+sudo apt install python3-requests 
+sudo apt install base58 -y 
 
-echo "Installing selenium for website injections"
-pip install selenium
+echo "Verifying installation..."
+sudo python3 -c "import hashlib; import base58; import binascii; from ecdsa import SigningKey, SECP256k1; import json; import base64; import getpass; import requests; from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes; from cryptography.hazmat.backends import default_backend; from cryptography.hazmat.primitives import padding; import secrets"
 
-
-# Install ngrok using snap
-echo "Installing ngrok..."
-sudo snap install ngrok
-
-# Install Node.js
-echo "Installing node.js..."
-sudo apt install -y nodejs npm
-npm install
-npm install express multer
-
-npm init -y
-npm install express multer
-
-# Install Discord Bot library
-echo "Installing discord.py..."
-pip3 install discord.py
-
-# Install other utilities
-echo "Installing whois..."
-sudo apt-get install -y whois
-echo "Installing bs4"
-pip3 install beautifulsoup4
-
-echo "Installation complete."
+if [ $? -eq 0 ]; then
+    echo "All dependencies installed successfully."
+else
+    echo "There was an error in installation."
+fi
